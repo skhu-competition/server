5fbcc70b83bbd447ae157785835288e208acfa5d
bomikim0214 <rlaqhals05@naver.com>
2025-03-29 20:49:26 +0900

    testcontroller

---
+++ src/main/java/server/test/TestController.java
@@ -0,0 +1,13 @@
+package server.test;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @PostMapping("test")
+    public String test(){
+        return "test 성공";
+    }
+}

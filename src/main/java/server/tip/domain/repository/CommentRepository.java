322aab1ff5a68e7e46bd39a8bbcbc3e65fe5ce51
Park, Daekyeong <pdk2037@gmail.com>
2025-04-10 18:44:27 +0900

    쿼리 수정 22

--- src/main/java/server/tip/domain/repository/CommentRepository.java
+++ src/main/java/server/tip/domain/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package server.tip.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import server.tip.domain.Comment;
 import server.tip.domain.Post;
 
@@ -13,5 +14,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Modifying
     @Query("delete from Comment c where c.post = :post")
-    Void deleteByPost(Post post);
+    void deleteByPost(@Param("post") Post post);
 }
